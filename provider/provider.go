22ca18f0de707dee630b7e565b5983e5fb7565f3
Edson Michaque <edson@michaque.com>
2022-05-22 13:26:43 +0200

    simplify provider interface

--- provider/provider.go
+++ provider/provider.go
@@ -4,35 +4,11 @@ import "io"
 
 type Builder func() Provider
 
-type ConfigReader interface {
-	ParseConfig(io.Reader) error
-}
-
-type MessageScheduler interface {
-	ScheduleMessages(string, Request) Response
-}
-
-type MessageSender interface {
-	SendMessages(Request) Response
-}
-
-type MessageQuerier interface {
-	QueryMessagesStatus(Request) Response
-}
-
-type MessageReceiver interface {
-	ReceiveMessages(Request) Response
-}
-
-type Capabilities interface {
-	Capabilities() []Capability
-}
-
 type Provider interface {
-	ConfigReader
-	MessageSender
-	MessageQuerier
-	MessageReceiver
-	MessageScheduler
-	Capabilities
+	ParseConfig(io.Reader) error
+	SendMessages(Request) Response
+	QueryMessagesStatus(Request) Response
+	ReceiveMessages(Request) Response
+	ScheduleMessages(string, Request) Response
+	Capabilities() []Capability
 }
